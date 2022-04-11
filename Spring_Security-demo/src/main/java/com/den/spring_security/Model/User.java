51d571b52d578ac4f6208d6b6167904227525426
Denis <svd.denis@mail.ru>
2022-04-11 10:47:44 +0300

    chance

--- Spring_Security-demo/src/main/java/com/den/spring_security/Model/User.java
+++ Spring_Security-demo/src/main/java/com/den/spring_security/Model/User.java
@@ -21,7 +21,7 @@ public class User implements UserDetails {
     @Transient
     private String passwordConfirm;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "users_id"),
